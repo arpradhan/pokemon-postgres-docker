30a87a6dfdc6aebe5c17078d37bf31590cbf7c66
Abhijit Pradhan <arpradhan@users.noreply.github.com>
2019-08-18 17:48:30 -0400

    Add pokemon_stats tables (#1)
    * Add pokemon_stats table
    
    * Add stats table
    
    * Add missing semicolon

--- ddl/load_tables.sql
+++ ddl/load_tables.sql
@@ -1,2 +1,8 @@
 copy pokemon
 from '/seed/pokemon.csv' delimiter ',' csv header;
+
+copy pokemon_stats (pokemon_id, stat_id, base_stat, effort)
+from '/seed/pokemon_stats.csv' delimiter ',' csv header;
+
+copy stats
+from '/seed/stats.csv' delimiter ',' csv header;
